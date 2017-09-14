75467ed1d2a0b32ff68c017175a33f4782b1d1a7
Shelby Switzer <shelbyswitzer@gmail.com>
2017-09-14 10:47:54 -0400

    Update with new file_grabber location
    Also change comments to logs to be more useful

--- api/deploy/start.sh
+++ api/deploy/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# sleep because compose doesn't wait to make sure elastic is ready
+echo "sleeping because compose doesn't wait to make sure elastic is ready"
 sleep 8
 
-# run file grabber
-ruby /api/data_processor/file_grabber.rb
+echo "running the file grabber"
+ruby /tmp/file_grabber.rb
 
-# start API server
-bundle exec rackup -p 4567 -o 0.0.0.0
+echo "starting the API server with live reload"
+rerun "bundle exec rackup -p 4567 -o 0.0.0.0"
